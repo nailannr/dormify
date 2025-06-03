1cb0e55b7ca7752e22e9b93284f47bbfae7232dd
nailannr <nailanausheen5@gmail.com>
2025-06-03 22:43:36 +0600

    Authentication w\o jwt

--- Client/src/components/user/signup.jsx
+++ Client/src/components/user/signup.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { useNavigate } from "react-router-dom";
 import "./Signup.css";
+import API from '../../api'
 
 export default function Signup() {
+  const [form,setForm] = useState({name:'', email:'', password:''});
   const navigate = useNavigate();
 
-  function handleSubmit(e) {
+  const  handleSubmit = async (e)=> {
     e.preventDefault();
-    // Simulate successful signup
-    navigate("/user/home");
+    console.log("Form Data:",form)
+    try {
+      const res = await API.post('/auth/signup', form);
+      alert(res.data.message);
+      navigate("/user/home");
+    } catch (err) {
+      alert(err.response.data.message);
+    }
+    
   }
 
   return (
     <div className="auth-container">
       <img src="/sust-logo.png" alt="SUST Logo" className="logo" />
       <h2>Sign Up</h2>
-      <form className="auth-form" onSubmit={handleSubmit}>
-        <input type="text" placeholder="Username" required />
-        <input type="password" placeholder="Password" required />
-        <input type="password" placeholder="Confirm Password" required />
+      <form 
+      // action="/user/signup"
+      // method="post"
+      className="auth-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" name= "name" onChange={e => setForm({ ...form, name: e.target.value })} required />
+        <input type="email" placeholder="Email" name= "email" onChange={e => setForm({ ...form, email: e.target.value })} required />
+        <input type="password" placeholder="Password" name= "password" onChange={e => setForm({ ...form, password: e.target.value })} required />
         <button type="submit">Sign Up</button>
         <p>
           Already have an account?{" "}
---
+++ server/routes/user.routes.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router();
+
+
+router.post('/signup', (req,res) =>{
+    console.log(req.body)
+    res.json({message: 'user registered successfully'})
+})
+
+module.exports = router;
