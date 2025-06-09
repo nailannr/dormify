6a3f1b6ad429acdc56bf37d812a361a40e1d654b
Kamrun Sumaiya <106950990+2020331020Kamrun@users.noreply.github.com>
2025-06-10 00:17:08 +0600

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/user/login.jsx";
 import Signup from "./components/user/signup.jsx";
 import Home from "./components/user/home.jsx";
-import BookSeat from "./components/user/bookseat.jsx";
-import ComplainBox from "./components/user/complainBox.jsx";
+import BookSeat1 from "./components/user/BookSeat1.jsx";
+import BookSeat2 from "./components/user/BookSeat2.jsx";
+import BookSeat3 from "./components/user/BookSeat3.jsx";
+import ComplainBox from "./components/user/ComplainBox.jsx";
 import HallNotice from "./components/user/hallNotice.jsx";
 import MakePayment from "./components/user/makePayment.jsx";
 import SeatCancellation from "./components/user/seatCancel.jsx";
-import Update from "./components/user/update.jsx";
-import AvailableSeats from "./components/user/AvailableSeat.jsx";
+import Update from "./components/user/ApplicationStatus.jsx";
 
 function App() {
   return (
@@ -19,13 +20,15 @@ function App() {
         <Route path="/user/login" element={<Login />} />
         <Route path="/user/signup" element={<Signup />} />
         <Route path="/user/home" element={<Home />} />
-        <Route path="/user/bookseat" element={<BookSeat />} />
-        <Route path="/user/complainBox" element={<ComplainBox />} />
+        <Route path="/user/BookSeat1" element={<BookSeat1 />} />
+         <Route path="/user/BookSeat2" element={<BookSeat2 />} />
+         <Route path="/user/BookSeat3" element={<BookSeat3 />} />
+        <Route path="/user/ComplainBox" element={<ComplainBox />} />
         <Route path="/user/hallNotice" element={<HallNotice />} />
         <Route path="/user/makePayment" element={<MakePayment />} />
         <Route path="/user/seatCancel" element={<SeatCancellation />} />
-        <Route path="/user/update" element={<Update />} />
-        <Route path="/user/AvailableSeat" element={<AvailableSeats />} />
+        <Route path="/user/ApplicationStatus" element={<ApplicationStatus />} />
+        
       </Routes>
     </Router>
   );
